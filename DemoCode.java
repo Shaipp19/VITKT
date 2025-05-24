fdf85d1d598535b550e9b9273ff7597254f79e7e
shaipp19 <shaileshpp19@gmail.com>
2025-05-24 11:21:37 +0530

    Code for multiplication is added -> DemoCode.java

--- DemoCode.java
+++ DemoCode.java
@@ -4,9 +4,21 @@ class DemoCode(String args[]){
 int a=20;
 int b=30;
 
-//Make addition of 2 numbers
+//Make addition of 2 numbersgit 
 int c = a+b;
 
 System.out.println("Addition is c =: "+c);
 
+//New code for division
+
+c= a/b; //Fixed devide by 0 error 
+
+System.out.println("Division c = "+c);
+
+//Dev1 Add his code 20 lines 
+
+//Add multiplication code 
+c = a*b;
+
+System.out.println("Multiplication c = "+c);
 }
